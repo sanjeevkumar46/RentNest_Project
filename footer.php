2a3b6a86192ba719af3f2a75900382c59ab54beb
Saurabh Kumar <saurabh.kumar133275@marwadiuniversity.ac.in>
2026-09-09 13:57:28 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,15 @@
+<footer>
+
+    <p>
+
+        © 2026 RentNest
+
+        All Rights Reserved
+
+    </p>
+
+</footer>
+
+</body>
+
+</html>
---
+++ navbar.php
@@ -0,0 +1,23 @@
+<header>
+
+    <div class="logo">
+        <h1>RentNest</h1>
+    </div>
+
+    <nav>
+
+        <a href="/RentNest/index.php">Home</a>
+
+        <a href="/RentNest/rooms.php">Rooms</a>
+
+        <a href="/RentNest/about.php">About</a>
+
+        <a href="/RentNest/contact.php">Contact</a>
+
+        <a href="/RentNest/login.php">Login</a>
+
+        <a href="/RentNest/register.php">Register</a>
+
+    </nav>
+
+</header>
